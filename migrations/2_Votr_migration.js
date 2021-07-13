02a14b102552d377d548bd33744e187069393997
MrKampla <mrkampla@gmail.com>
2021-07-13 20:58:13 +0200

    Update migration

--- migrations/2_Votr_migration.js
+++ migrations/2_Votr_migration.js
@@ -1,7 +1,13 @@
 const VotrPollFactory = artifacts.require('VotrPollFactory');
 const FirstPastThePostPollTypeContract = artifacts.require('FirstPastThePostPollType');
+const CumulativePollTypeContract = artifacts.require('CumulativePollType');
+const EvaluativePollTypeContract = artifacts.require('EvaluativePollType');
+const QuadraticPollTypeContract = artifacts.require('QuadraticPollType');
 
 module.exports = function (deployer) {
   deployer.deploy(VotrPollFactory);
   deployer.deploy(FirstPastThePostPollTypeContract);
+  deployer.deploy(CumulativePollTypeContract);
+  deployer.deploy(EvaluativePollTypeContract);
+  deployer.deploy(QuadraticPollTypeContract);
 };
